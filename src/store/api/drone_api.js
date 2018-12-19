9401168d0cc3c29e228cf1be918ee007d399dfc8
GaiSawant <grs7@rice.ed>
2018-12-20 01:04:38 +0530

    Drone data based files

---
+++ src/store/api/drone_api.js
@@ -0,0 +1,18 @@
+import "isomorphic-fetch";
+
+const drone = async () => {
+
+    console.log("Drone_api");
+
+    // Polling the drone Api
+    const response = await fetch(
+        `https://react-assessment-api.herokuapp.com/api/drone/`
+    );
+    if (!response.ok) {
+        return { error: { code: response.status } };
+    }
+    const json = await response.json();
+    return { data: json };
+};
+
+export default drone;
